4778f945defca2f88e20350da9b0d6bbca381f0d
hong yuseok <jericoh@naver.com>
2021-12-21 17:40:20 +0900

    dto validation 조건 정리

--- src/main/java/com/bankSystem/BankSystem/api/UserApiController.java
+++ src/main/java/com/bankSystem/BankSystem/api/UserApiController.java
@@ -4,17 +4,21 @@ import com.bankSystem.BankSystem.dto.UserSaveRequestDto;
 import com.bankSystem.BankSystem.dto.UserSaveResponseDto;
 import com.bankSystem.BankSystem.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserApiController {
     private final UserService userService;
 
-    @PostMapping("/api/user")
-    public UserSaveResponseDto save(@RequestBody UserSaveRequestDto userSaveRequestDto) {
+    @PostMapping("/add")
+    public UserSaveResponseDto save(@Validated @RequestBody UserSaveRequestDto userSaveRequestDto, BindingResult bindingResult) {
         return userService.save(userSaveRequestDto);
     }
 }
--- src/main/java/com/bankSystem/BankSystem/dto/UserSaveRequestDto.java
+++ src/main/java/com/bankSystem/BankSystem/dto/UserSaveRequestDto.java
@@ -5,17 +5,33 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 @Getter
 @NoArgsConstructor
 public class UserSaveRequestDto {
     // 모두 필수 적으로 들어가야하는 값
+
+    // 30자 넘지 않도록
     private String name;
+
+    // 날짜 형식에 맞는 validation 사용할 것
     private LocalDate birthDate;
+
+    // 200자 넘지 않도록
     private String address;
+
+    // 이메일 형식에 맞도록
+    // 30자 넘지 않도록
     private String email;
+
+    // password 사용할 수 있는지 없는 한번만 확인
     private String password;
+
+    // 중간 "-"없도록
+    // 숫자 30자 넘지 안호도록
     private String phoneNumber;
 
     @Builder
