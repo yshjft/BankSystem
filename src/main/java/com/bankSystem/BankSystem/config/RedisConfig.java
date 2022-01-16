f5c4399b2c8e05823373f5805a01a73ed2ccb4eb
hong yuseok <jericoh@naver.com>
2022-01-16 10:50:59 +0900

    환경에 따른 설정 파일 변경

--- src/main/java/com/bankSystem/BankSystem/config/RedisConfig.java
+++ src/main/java/com/bankSystem/BankSystem/config/RedisConfig.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -16,7 +17,7 @@ import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSeriali
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@Slf4j
+// @Profile("prod")
 @Configuration
 @EnableRedisHttpSession
 @RequiredArgsConstructor
